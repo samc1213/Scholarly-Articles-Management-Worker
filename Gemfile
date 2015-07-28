6da91badf99083a68e32708bef366c099d1c618d
Sam Cohen <samc1213@gmail.com>
2015-07-28 16:28:49 -0500

    attempting sablon

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 ruby "2.2.0"
 gem 'sinatra', '1.1.0'
+gem 'sablon'
--- web.rb
+++ web.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require 'sablon'
 
 get '/' do
   "Hello, world"
 end
+
+
+
+template = Sablon.template(File.expand_path("TestExecuteTemplate.docx"))
+
+context = {
+  firstname: "FIRSTY"
+  }
+
+template.render_to_file File.expand_path("output.docx"), context
+
+
