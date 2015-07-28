82d8c5682fb0463386df210408485ed47e3a31f4
Sam Cohen <samc1213@gmail.com>
2015-07-28 16:34:53 -0500

    trying more

--- web.rb
+++ web.rb
@@ -3,16 +3,14 @@ require 'sablon'
 
 get '/' do
   "Hello, world"
+      template = Sablon.template(File.expand_path("TestExecuteTemplate.docx"))
+    
+    context = {
+      firstname: "FIRSTY"
+      }
+    
+    template.render_to_file File.expand_path("output.docx"), context
 end
 
-
-
-template = Sablon.template(File.expand_path("TestExecuteTemplate.docx"))
-
-context = {
-  firstname: "FIRSTY"
-  }
-
-template.render_to_file File.expand_path("output.docx"), context
 
 
