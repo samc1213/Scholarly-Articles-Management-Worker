64a6648e90b5a24f0a54c077aaf458379a98b361
Sam Cohen <samc1213@gmail.com>
2015-07-29 12:52:06 -0500

    trying to just add file

--- web.rb
+++ web.rb
@@ -18,7 +18,7 @@ get '/' do
     
     file = File.open('output.docx')
     grid_file = Mongo::Grid::File.new(file.read, :filename => File.basename(file.path))
-     # client[:queue].insert_one({message: 'whatsgoodmofoszzzzz', file: grid_file})
+     client[:files].insert_one(grid_file)
 
     send_file "output.docx"
 
