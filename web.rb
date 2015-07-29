4b143432d57ed1e112cb57ae4f1ba1400f7d7501
Sam Cohen <samc1213@gmail.com>
2015-07-29 13:14:36 -0500

    step by step...

--- web.rb
+++ web.rb
@@ -4,8 +4,8 @@ require 'mongo'
 
 get '/' do
     mongo_uri = "mongodb://heroku_v7w2qftd:a5h7slci8p0b2p9nt7qe96hmvv@ds027483.mongolab.com:27483/heroku_v7w2qftd"
-    client = Mongo::Client.new(mongo_uri, :database => 'heroku_v7w2qftd')
-    client[:queue].insert_one({message: 'whatsgoodmofos'})
+    db = Mongo::Client.new(mongo_uri, :database => 'heroku_v7w2qftd')
+    db[:queue].insert_one({message: 'whatsgoodmofos'})
 
 
     template = Sablon.template(File.absolute_path("TestExecuteTemplate.docx"))
@@ -16,10 +16,8 @@ get '/' do
     
     template.render_to_file File.absolute_path("output.docx"), context
        
-    file = File.open('output.docx')
-    grid = Mongo::Grid.new(client)
-    Mongo::Grid.put(file)
-
+    @grid = Grid.new(db)
+    
     send_file "output.docx"
 
      return "Hello, world"
