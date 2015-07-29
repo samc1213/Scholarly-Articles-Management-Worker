356d6b02a899a2f2c987ee2dfa2234c715285a32
Sam Cohen <samc1213@gmail.com>
2015-07-29 13:59:57 -0500

    fuck mongo

--- web.rb
+++ web.rb
@@ -1,12 +1,12 @@
 require 'sinatra'
 require 'sablon'
 require 'mongo'
-include Mongo
+require 'aws-sdk'
 
 get '/' do
     mongo_uri = "mongodb://heroku_v7w2qftd:a5h7slci8p0b2p9nt7qe96hmvv@ds027483.mongolab.com:27483/heroku_v7w2qftd"
-    @db = ::MongoClient.new(mongo_uri).db('heroku_v7w2qftd')
-    @db[:queue].insert_one({message: 'whatsgoodmofos'})
+    db = Mongo::Client.new(mongo_uri).db('heroku_v7w2qftd')
+    db[:queue].insert_one({message: 'whatsgoodmofoszzzzz'})
 
 
     template = Sablon.template(File.absolute_path("TestExecuteTemplate.docx"))
@@ -17,14 +17,14 @@ get '/' do
     
     template.render_to_file File.absolute_path("output.docx"), context
        
-    grid = Mongo::Grid.new(db)
     
     doc = File.open("output.docx")
-    id2   = @grid.put(doc, :filename => "me.jpg")
+    s3 = Aws::S3::Resource.new(region:'us-west-2')
+    obj = s3.bucket('cpgrantsdocs').object('docdoc')
+    obj.upload_file('output.docx')
+
     
-    return id2
-    
-    # send_file "output.docx"
+    send_file "output.docx"
 
      # return "Hello, world"
 end
