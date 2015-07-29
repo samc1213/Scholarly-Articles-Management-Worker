0525778afe01445c1350e92f7d7c0271798afec9
Sam Cohen <samc1213@gmail.com>
2015-07-29 12:25:30 -0500

    testing mongo

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 ruby "2.2.0"
 gem 'sinatra', '1.1.0'
 gem 'sablon'
+gem 'mongo', '~> 2.0'
--- web.rb
+++ web.rb
@@ -1,9 +1,14 @@
 require 'sinatra'
 require 'sablon'
+require 'mongo'
 
 get '/' do
+    mongo_uri = "mongodb://heroku_v7w2qftd:a5h7slci8p0b2p9nt7qe96hmvv@ds027483.mongolab.com:27483/heroku_v7w2qftd"
+    client = Mongo::Client.new(mongo_uri, :database => 'heroku_v7w2qftd')
+    client[:queue].insert_one({message: 'whatsgoodmofos'})
 
-      template = Sablon.template(File.absolute_path("TestExecuteTemplate.docx"))
+
+    template = Sablon.template(File.absolute_path("TestExecuteTemplate.docx"))
     
     context = {
       firstname: "FIRSTY"
@@ -12,7 +17,6 @@ get '/' do
     template.render_to_file File.absolute_path("output.docx"), context
 
     send_file "output.docx"
-
 
      return "Hello, world"
 end
