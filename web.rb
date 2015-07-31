0333563d1041da874b05296e6b562b036897873a
Sam Cohen <samc1213@gmail.com>
2015-07-31 11:59:35 -0500

    modified post data

--- web.rb
+++ web.rb
@@ -4,20 +4,27 @@ require 'mongo'
 require 'aws-sdk'
 
 post '/jobs' do
-    
     id = params[:id]
-    mongo_uri = "mongodb://heroku_v7w2qftd:a5h7slci8p0b2p9nt7qe96hmvv@ds027483.mongolab.com:27483/heroku_v7w2qftd"
-    db = Mongo::Client.new(mongo_uri, :database => 'heroku_v7w2qftd')
+    data = params[:data]
+    rubydata = JSON.parse(data)
     
-    job = db[:messages].find({:_id => BSON::ObjectId(id)}).find_one_and_update('$set' => {done: 'true'})
+    name = rubydata[0]["name"]
     
     template = Sablon.template(File.absolute_path("TestExecuteTemplate.docx"))
     
     context = {
-      firstname: "FIRSTY"
+      firstname: name
       }
     
-    template.render_to_file File.absolute_path("output.docx"), context
+    template.render_to_file File.absolute_path("output.docx"), context    
     
-    send_file 'output.docx'
+    s3 = Aws::S3::Resource.new(region:'us-west-2', credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']))
+    obj = s3.bucket('cpgrantsdocs').object(id)
+    obj.upload_file('output.docx')
+
+     status 200
+     body 'DONE'
 end
+
+
+
