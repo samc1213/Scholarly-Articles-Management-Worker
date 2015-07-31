fc654b09d738dda12676b445e99bacc8b57e9a6c
Sam Cohen <samc1213@gmail.com>
2015-07-31 16:03:24 -0500

    all i wanna do is debug

--- web.rb
+++ web.rb
@@ -17,22 +17,22 @@ post '/jobs' do
     puts rubydata.inspect
     puts rubydata.class
     
-    each rubydata do |rd|
-      newgrant = grant.new(rd["name"], rd["status"], rd["source"], rd["amount"], rd["awardperiod1"], rd["awardperiod2"], rd["piamount"], rd["specify"], rd["description"])
-      grantarray.push(newgrant)
-    end
-    
-    
-    
-    context = {
-      grants: grantarray
-    }
-    
-    template.render_to_file File.absolute_path("output.docx"), context    
-    
-    s3 = Aws::S3::Resource.new(region:'us-west-2', credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']))
-    obj = s3.bucket('cpgrantsdocs').object(id)
-    obj.upload_file('output.docx')
+    # each rubydata do |rd|
+      # newgrant = grant.new(rd["name"], rd["status"], rd["source"], rd["amount"], rd["awardperiod1"], rd["awardperiod2"], rd["piamount"], rd["specify"], rd["description"])
+      # grantarray.push(newgrant)
+    # end
+#     
+#     
+#     
+    # context = {
+      # grants: grantarray
+    # }
+#     
+    # template.render_to_file File.absolute_path("output.docx"), context    
+#     
+    # s3 = Aws::S3::Resource.new(region:'us-west-2', credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']))
+    # obj = s3.bucket('cpgrantsdocs').object(id)
+    # obj.upload_file('output.docx')
 
      status 200
      body 'DONE'
